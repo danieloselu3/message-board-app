fffebd0c3888b8402daf8d69167b2281de083855
danieloselu3 <doselu3@gmail.com>
2021-12-20 12:24:14 +0300

    modified homepage view to retrieve posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from.models import Post
+
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'posts/home.html')
+    all_text = Post.objects.all()
+    return render(request, 'posts/home.html', {
+        'all_posts_list':all_text,
+    })
