e6ff333cb105bf0aea19cc6b57ac837c9dfc44a3
danieloselu3 <doselu3@gmail.com>
2021-12-20 13:12:17 +0300

    added homepage view tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,21 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 
 # Create your tests here.
 
-class RoutingTests(TestCase):
+class HomepageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='just a test')
     
-    def test_home_page_routing(self):
+    def test_view_url_exists_at_the_proper_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('posts:home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('posts:home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'posts/home.html')
 
 class PostModelTest(TestCase):
 
     def setUp(self):
-        Post.objects.create(text= "just a test")
+        Post.objects.create(text= "this is another test")
 
     def test_text_context(self):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
-        self.assertEqual(expected_object_name, 'just a test')
+        self.assertEqual(expected_object_name, 'this is another test')
