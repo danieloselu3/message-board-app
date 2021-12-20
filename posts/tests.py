beb6dce848b79e2bd795e9d6568f0bb54f463386
danieloselu3 <doselu3@gmail.com>
2021-12-20 12:41:17 +0300

    added test for the Post model

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from .models import Post
+
 # Create your tests here.
+
+class RoutingTests(TestCase):
+    
+    def test_home_page_routing(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text= "just a test")
+
+    def test_text_context(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
